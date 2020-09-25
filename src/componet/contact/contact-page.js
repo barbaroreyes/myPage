1b18f356ee25a9b2d9e7430e2ae23b96e1edaf7a
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-25 02:28:57 -0500

    fixing contact-Page

--- src/componet/contact/contact-page.js
+++ src/componet/contact/contact-page.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-// import{Grid,Cell} from 'react-mdl';
+import{Grid,Cell} from 'react-mdl';
 import './contact-styles.scss'
 
 export default class Contact extends Component {
     render() {
         return (
             <div className='contact-body'>
-                {/* <Grid className='cotact-grid'>
+                <Grid className='cotact-grid'>
                     <Cell col={6}>Half Page</Cell>
                     <Cell col={6}>Half Page</Cell>  
-                </Grid> */}
+                </Grid>
                 <h1>Hello</h1>
             </div>
         )
