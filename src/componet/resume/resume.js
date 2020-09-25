68bf0465c6763af1d59c0b2523cc89af783281bb
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-25 08:54:38 -0500

    added a homme link

--- src/componet/resume/resume.js
+++ src/componet/resume/resume.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {Grid,Cell} from 'react-mdl'
 
 export default function resume() {
     return (
         <div>
-            <h1>Resume Page</h1>
+            <Grid>
+                <Cell col={4}>
+                <h1>Resume Page</h1>
+                </Cell>
+                <Cell col={4}>
+                <h1>Resume Page</h1>
+                </Cell>
+                <Cell col={4}>
+                <h1>Resume Page</h1>
+                </Cell>
+
+            </Grid>
+            
         </div>
     )
 }
