ae8a95483465f8ac8a32dc5d21dcd07e16d0241c
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-28 17:00:29 -0500

    added btns to nav

--- src/App.js
+++ src/App.js
@@ -6,21 +6,25 @@ import Main from './componet/MAin/main';
  
  export default class App extends Component {
    render() {
-     return ( <main>
-       <div>
-         <div className="content" style={{background:'linear-gradient(to left ,rgb(240, 122, 122) ,rgb(204, 214, 230))'}}>
-    
-            <nav style={{background:'grey',height:'100px',width:'100%',display:'flex',justifyContent:'center',textAlign:'center'}}>
-                < Link className='nav' to ="/">Home</Link>
-                < Link className='nav' to ="/aboutMe">AboutMe</Link>
-                < Link className='nav' to ="/resume">Resume</Link>
-                < Link className='nav' to ="/contact">Contact</Link>
-                < Link className='nav' to ="/projects">Proyects</Link>
-            </nav>
-         <div className="page-content" />
-            <Main/>
-         </div>
-      </div>
+     return ( 
+        <main>
+        <div className="content">
+              <nav style={{height:'50px'}}>
+                <button className='btn'>< Link className='nav' to ="/">Home</Link></button>
+                <button className='btn'>< Link className='nav' to ="/aboutMe">AboutMe</Link>< Link className='nav' to ="/aboutMe">AboutMe</Link></button>
+                <button className='btn'>< Link className='nav' to ="/resume">Resume</Link></button>
+                <button className='btn'> < Link className='nav' to ="/contact">Contact</Link></button>
+                <button className='btn'> < Link className='nav' to ="/projects">Proyects</Link></button>
+                
+                
+                
+               
+               
+              </nav>
+              <Main/>
+         <div/>
+           
+         </div>    
       </main>
      )
    }
--- src/componet/contact/contact-page.js
+++ src/componet/contact/contact-page.js
@@ -2,50 +2,44 @@ import React, { Component } from 'react';
 import{} from 'react-mdl';
 import './contact-style.scss';
 import work from '../../work.jpg';
-import{Grid,Cell,ListItem,List,ListItemContent} from 'react-mdl'
+
 
 
     export default class Contact extends Component {
         render() {
             return (
          <div className='contact-body'>
-                <Grid className='contact-grid'>
-                        <Cell col={6}>
-                            <div>
-                            <h1>Barbaro Reyes</h1>
-                            <img className='pict' src={work}
-                            alt='avatar'
-                            style={{height:'250px'}}
-                            />
-                            <div> <p style={{width:'75%',margin:'auto',paddingTop:'1em'}}>With a huge desire to bring smart ideas to the Digital World</p></div>
-         </div>    
-                        </Cell>
-                   <Cell col={6}>
-                            <h4>Contact me</h4>
-                            <hr/>
+                            <div style={{display:'flex',flexDirection:'column'}}>
+                            <h1 style={{marginLeft:'20px'}}>Barbaro Reyes</h1>
+                            <img className='pict' src={work} alt='avatar'style={{height:'350px',marginLeft:'10px'}}/>
+                             <p style={{width:'50%',margin:'auto',paddingTop:'1em'}}>With a huge desire to bring smart ideas to the Digital World</p>
+                          </div>    
+                   
+                            <h6>Contact me</h6>
+                           
                     <div className='contact-list'>
-                        <List>
-                             <ListItem>
-                                <ListItemContent style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
-                                <i className="fas fa-phone-square">(512)6086054</i></ListItemContent>
-                            </ListItem>
-                            <ListItem>
-                                <ListItemContent style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
+                        <ul>
+                             <li>
+                                <article style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
+                                <i className="fas fa-phone-square">(512)6086054</i></article>
+                            </li>
+                            <li>
+                                <article style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
                                 <i class="fas fa-envelope-square"><a href='barbarotrabajo@gmail.com'>barbarotrabajo@gmail.com</a></i>
-                                </ListItemContent>
-                            </ListItem>
-                            <ListItem>
-                                <ListItemContent 
+                                </article>
+                            </li>
+                            <li>
+                                <article
                                  style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
                                 <i class="fab fa-github"><a href ='https://github.com/barbaroreyes'>https://github.com/barbaroreyes</a></i>
-                                </ListItemContent>
-                            </ListItem>
-                            <ListItem>
-                                <ListItemContent 
+                                </article>
+                            </li>
+                            <li>
+                                <article
                                  style={{fontSize:'25px',fontFamily:'Averia Gruesa Libre'}} >
                                  <i className=" link fab fa-linkedin"><a href='https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/'>https://www.linkedin.com/in/barbaro-reyes-b023aa1a7/</a></i>
-                                </ListItemContent>
-                            </ListItem>
+                                </article>
+                            </li>
                             <div className='socials-liks'>
                               {/* linkedIn */}
                                     <a  className ='link' href="https://www.linkedin.com/in/barbaro-reyes" rel='noopener noreferrer' target='_black'>
@@ -55,16 +49,9 @@ import{Grid,Cell,ListItem,List,ListItemContent} from 'react-mdl'
                                       <a href="https://github.com/barbaroreyes" rel='noopener noreferrer' target='_black'>
                                       <i className="fab fa-github-square"></i>
                                     </a>
-                                    
-
                          </div>
-                            
-                            
-                            
-                        </List>
-                    </div>
-                   </Cell>  
-                </Grid>
+                        </ul>
+                    </div>     
             </div>
             )
         }
