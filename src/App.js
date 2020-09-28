2067b8cef2189d7f617be17fdb18612c4adea730
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-28 15:56:33 -0500

    added  styles to about

--- src/App.js
+++ src/App.js
@@ -8,26 +8,18 @@ import Main from './componet/MAin/main';
    render() {
      return ( <main>
        <div>
-         <div className="demo-big-content">
+         <div className="content" style={{background:'linear-gradient(to left ,rgb(240, 122, 122) ,rgb(204, 214, 230))'}}>
     
-            <nav style={{background:'grey',margin:'10px',width:'100%',display:'flex',justifyContent:'center'}}>
+            <nav style={{background:'grey',height:'100px',width:'100%',display:'flex',justifyContent:'center',textAlign:'center'}}>
                 < Link className='nav' to ="/">Home</Link>
                 < Link className='nav' to ="/aboutMe">AboutMe</Link>
                 < Link className='nav' to ="/resume">Resume</Link>
                 < Link className='nav' to ="/contact">Contact</Link>
                 < Link className='nav' to ="/projects">Proyects</Link>
             </nav>
-        
-        
-            
-        
-        
-            <div className="page-content" />
+         <div className="page-content" />
             <Main/>
-            
-       
-    
-           </div>
+         </div>
       </div>
       </main>
      )
--- src/componet/about/aboutMe.js
+++ src/componet/about/aboutMe.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function About() {
     return (
-        <section className='tc shadow-5  dib' style={{height:'250px'}}>
+        <section className='dib' style={{height:'250px',boxShadow:'3px 2px'}}>
             <article><h1>Hello, I'm Barbaro and after having graduated from technical school in Cuba for a long time, my job was to fix desktop computers, but from a very young age I felt the desire to achieve through programming logic make my dreams come true but until now I had been able For reasons contrary to my will, since I really enjoy working with groups of people who are on the same page, therefore I have nothing left to say, thank you very much  <a href='https://www.thinkful.com/bootcamp/web-development/flexible/'>ThinkFull</a> for giving me the opportunity</h1></article>
         </section>
     )
