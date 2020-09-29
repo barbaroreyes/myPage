899fc3966e87e4028fc1b6abdb7f40a4a8a4bcdd
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-28 19:35:12 -0500

    added bg color to btn

--- src/App.js
+++ src/App.js
@@ -10,16 +10,11 @@ import Main from './componet/MAin/main';
         <main>
         <div className="content">
               <nav style={{height:'50px'}}>
-                <button className='btn'>< Link className='nav' to ="/">Home</Link></button>
-                <button className='btn'>< Link className='nav' to ="/aboutMe">AboutMe</Link>< Link className='nav' to ="/aboutMe">AboutMe</Link></button>
-                <button className='btn'>< Link className='nav' to ="/resume">Resume</Link></button>
-                <button className='btn'> < Link className='nav' to ="/contact">Contact</Link></button>
-                <button className='btn'> < Link className='nav' to ="/projects">Proyects</Link></button>
-                
-                
-                
-               
-               
+                <button className='btn1'>< Link className='nav' to ="/">Home</Link></button>
+                <button className='btn1'>< Link className='nav' to ="/aboutMe">AboutMe</Link></button>
+                <button className='btn1'>< Link className='nav' to ="/resume">Resume</Link></button>
+                <button className='btn1'> < Link className='nav' to ="/contact">Contact</Link></button>
+                <button className='btn1'> < Link className='nav' to ="/projects">Proyects</Link></button>                          
               </nav>
               <Main/>
          <div/>
