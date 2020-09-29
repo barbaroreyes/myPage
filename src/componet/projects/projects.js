3dd47ec31c1d0cf7e8126f2bb55dba0f9adead97
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-28 20:14:22 -0500

    added projects links

--- src/componet/projects/projects.js
+++ src/componet/projects/projects.js
@@ -1,108 +1,19 @@
-import React, { Component } from 'react';
-import {Tabs,Tab,Grid,Cell,Card,CardTitle,CardText,CardActions,Button,CardMenu}from 'react-mdl';
-   
+import React, { Component } from 'react'
 
 
-
-
-export default class Projects extends Component {
-    constructor(props){
-     super(props)
-     this.state ={
-         activeTab:0};
-    }
-    toggleCategories(){
-        if(this.state.activeTab===0){
-         return(<div>
-           <Card shadow={5} style={{width:'450',margin:'auto'}}>
-                <CardTitle style={{color:'#fff',height:'176px',background:'url(https://img-a.udemycdn.com/course/750x422/1199058_8435_3.jpg)center/cover'}}></CardTitle>
-                <CardText className='tc'>{}</CardText>
-                <CardMenu className='ma2'><a href='https://barbaroreyes.github.io/First-Responsive-WebsiteBarbaro/'>Firts REsponsive</a></CardMenu>
-                <CardMenu className='ma4'><a href='https://barbaroreyes.github.io/tablas/'>Tablas</a></CardMenu>
-               
-                <CardActions border>
-                   <Button colored><a href='https://github.com/barbaroreyes/First-Responsive-WebsiteBarbaro'>Github</a></Button>
-                   <Button colored>CodePen</Button>
-                   <Button colored>LiveDemo</Button>
-                </CardActions>
-            </Card>  
-        </div> )
-
-        }else if(this.state.activeTab ===1)
-       return (<div>
-        <Card  shadow={5} style={{width:'450',margin:'auto'}}>
-             <CardTitle style={{color:'#fff',height:'176px',background:'url(https://img.icons8.com/clouds/2x/react.png)center/cover'}}></CardTitle>
-             <CardText className='tc'>{}</CardText>
-             <CardMenu className='ma2'><a href='https://final-robots.netlify.app/'>Robots Fiends</a></CardMenu>
-             <CardMenu className='ma4'><a href='https://barbaro-store.netlify.app/'>Ecomerce</a></CardMenu>
-             <CardActions border>
-             <Button colored><a href='https://github.com/barbaroreyes/finalRobot'>Github</a></Button>
-                <Button colored>CodePen</Button>
-                <Button colored>LiveDemo</Button>
-             </CardActions>
-         </Card>  
-     </div>)
-
-       else if (this.state.activeTab===2){
-        return (<div>
-            <Card  shadow={5} style={{width:'450',margin:'auto'}}>
-                 <CardTitle style={{color:'#fff',height:'176px',background:'url(https://cdn01.alison-static.net/courses/1200/alison_courseware_intro_1200.jpg)center/cover'}}></CardTitle>
-                 <CardText className='tc'>{}</CardText>
-                 <CardActions border>
-                    <Button colored>Github</Button>
-                    <Button colored>CodePen</Button>
-                    <Button colored>LiveDemo</Button>
-                 </CardActions>
-             </Card>  
-         </div>) 
-            }
-            else if (this.state.activeTab===2){
-                return (<div>
-                    <Card  shadow={5} style={{width:'450',margin:'auto'}}>
-                         <CardTitle style={{color:'#fff',height:'176px',background:'url(https://cdn01.alison-static.net/courses/1200/alison_courseware_intro_1200.jpg)center/cover'}}></CardTitle>
-                         <CardText className='tc'>{}</CardText>
-                         <CardActions border>
-                            <Button colored>Github</Button>
-                            <Button colored>CodePen</Button>
-                            <Button colored>LiveDemo</Button>
-                         </CardActions>
-                     </Card>  
-                 </div>) 
-                    }
-            else{
-                return(<div>
-                    <Card  shadow={5} style={{width:'450',margin:'auto'}}>
-                         <CardTitle style={{color:'#fff',height:'176px',background:'url(https://techcommunity.microsoft.com/t5/image/serverpage/image-id/184300iC067819AE50300F4/image-size/medium?v=1.0&px=400)center/cover'}}></CardTitle>
-                         <CardText className='tc'>{}</CardText>
-                         <CardActions border>
-                            <Button colored>Github</Button>
-                            <Button colored>CodePen</Button>
-                            <Button colored>LiveDemo</Button>
-                         </CardActions>
-                     </Card>  
-                 </div>)
-            }
-    }
+export default class projects extends Component {
     render() {
         return (
-            <div className='category-tabs'>
-              <Tabs activeTab ={this.state.activeTab} onChange={(tabId)=> this.setState({activeTab:tabId})} ripple>
-                   <Tab>JavaScript</Tab>
-                    <Tab>React</Tab>
-                    <Tab>Angular</Tab>
-                    <Tab>ViuJs</Tab>
-                    <Tab>MongoDB</Tab>
-                </Tabs>
-
-            <section className= 'project-grid'>
-                  <Grid className='proyect-grid'>
-                         <Cell col={12}>
-                         <div>{this.toggleCategories()}</div>
-                         </Cell>
-                    </Grid>                 
-                </section>
-                
-            </div>
+            <div>
+                <ol>
+                    <li><a href='https://final-robots.netlify.app/'>Robots-Friends</a></li>
+                    <li><a href ='https://barbaro-store.netlify.app'>Ecomerce Store</a></li>
+                    <li><a href='https://barbaroreyes.github.io/First-Responsive-WebsiteBarbaro/'>Welcome_page</a></li>
+                    <li><a href='https://barbaroreyes.github.io/NASA-Page/'>Contact-Nasa-Page</a></li>
+                </ol>
+           </div>
         )
     }
 }
+
+  
