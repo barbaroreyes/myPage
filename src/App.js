1088e03d3b222d924e5920cd251488535239165b
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-09-28 19:51:11 -0500

    project done

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ import Main from './componet/MAin/main';
         <div className="content">
               <nav style={{height:'50px'}}>
                 <button className='btn1'>< Link className='nav' to ="/">Home</Link></button>
-                <button className='btn1'>< Link className='nav' to ="/aboutMe">AboutMe</Link></button>
-                <button className='btn1'>< Link className='nav' to ="/resume">Resume</Link></button>
-                <button className='btn1'> < Link className='nav' to ="/contact">Contact</Link></button>
-                <button className='btn1'> < Link className='nav' to ="/projects">Proyects</Link></button>                          
+                <button className='btn1'>< Link target="_blank"  className='nav' to ="/aboutMe">AboutMe</Link></button>
+                <button className='btn1'>< Link target="_blank"  className='nav' to ="/resume">Resume</Link></button>
+                <button className='btn1'> < Link target="_blank"  className='nav' to ="/contact">Contact</Link></button>
+                <button className='btn1'> < Link target="_blank"  className='nav' to ="/projects">Proyects</Link></button>                          
               </nav>
               <Main/>
          <div/>
